a582111a5f54d26f3dc5e205b7f0668b88277195
Christopher Kolstad <git@chriswk.no>
2020-08-25 23:00:37 +0200

    Also install fish

--- run_once_00_install_packages.sh
+++ run_once_00_install_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-yay -S --noconfirm direnv emacs exa ripgrep starship tmux vim neovim xclip zsh zsh-autosuggestions zsh-fast-syntax-highlighting go
+yay -S --noconfirm direnv emacs exa ripgrep starship tmux vim neovim xclip zsh zsh-autosuggestions zsh-fast-syntax-highlighting go fish fisher
 if [[ "$XDG_CURRENT_DESKTOP" != '' ]]; then
   yay -S --noconfirm ffmpeg libva libva-utils libva-intel-driver alacritty-ligatures adobe-source-code-pro-fonts papirus-icon-theme visual-studio-code-bin
 fi
