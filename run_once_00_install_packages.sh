d5c24637a926fc8c9f6703d613f1c991eda27e94
Christopher Kolstad <git@chriswk.no>
2020-08-25 22:50:07 +0200

    Use alacritty with ligatures patch

--- run_once_00_install_packages.sh
+++ run_once_00_install_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 yay -S --noconfirm direnv emacs exa ripgrep starship tmux vim neovim xclip zsh zsh-autosuggestions zsh-fast-syntax-highlighting go
 if [[ "$XDG_CURRENT_DESKTOP" != '' ]]; then
-  yay -S --noconfirm ffmpeg libva libva-utils libva-intel-driver alacritty adobe-source-code-pro-fonts papirus-icon-theme visual-studio-code-bin
+  yay -S --noconfirm ffmpeg libva libva-utils libva-intel-driver alacritty-ligatures adobe-source-code-pro-fonts papirus-icon-theme visual-studio-code-bin
 fi
 
