82bbeab0559526a2cb6691ad8f7eb1440714b2b8
Christopher Kolstad <git@chriswk.no>
2020-08-27 17:07:33 +0200

    Define new functions for checking installed packages

--- run_once_00_install_packages.sh
+++ run_once_00_install_packages.sh
@@ -1,6 +1,47 @@
 #!/bin/bash -e
-yay -S --noconfirm direnv emacs exa ripgrep starship tmux vim neovim xclip zsh zsh-autosuggestions zsh-fast-syntax-highlighting go fish fisher
+_isInstalled() {
+  package="$1"
+  check="$(yay -Qs "${package}" | grep "local" | grep "${package} ")";
+  if [ -n "${check}" ]; then
+    echo 0;
+    return;
+  fi
+  echo 1;
+  return;
+}
+
+_install() {
+  package="$1"
+
+  if [[ $(_isInstalled "${package}") == 0 ]]; then
+    echo "${package} is already installed."
+  else
+    yay -S --noconfirm "${package}"
+  fi
+}
+
+_installMany() {
+  toInstall=();
+
+  for pkg; do
+    if [[ $(_isInstalled "${pkg}") == 0 ]]; then
+      echo "${pkg} is already installed."
+      continue;
+    fi
+    toInstall+=("${pkg}");
+  done
+
+  if [[ "${toInstall[@]}" == "" ]]; then
+    echo "All packages are already installed";
+    return;
+  fi
+  printf "Packages not installed:\n%s\n" "${toInstall[@]}";
+  yay -S --noconfirm "${toInstall[@]}"
+}
+
+packages=("direnv" "emacs" "exa" "ripgrep" "starship" "tmux" "vim" "neovim" "xclip" "zsh" "zsh-autosuggestions" "zsh-completions" "zsh-fast-syntax-highlighting" "go" "fish" "fisher")
 if [[ "$XDG_CURRENT_DESKTOP" != '' ]]; then
-  yay -S --noconfirm ffmpeg libva libva-utils libva-intel-driver alacritty-ligatures adobe-source-code-pro-fonts papirus-icon-theme visual-studio-code-bin
+  packages+=("ffmpeg" "libva" "libva-utils" "libva-intel-driver" "alacritty-ligatures" "adobe-source-code-pro-fonts" "papirus-icon-theme" "visual-studio-code-bin");
 fi
 
+_installMany "${packages[@]}"
