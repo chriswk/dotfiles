1539590522653efc660508feec7bbae12e4b4bbf
Christopher Kolstad <christopher.kolstad@mercell.com>
2020-09-18 11:43:39 +0200

    Update private_dot_config/fish/conf.d/paths.fish

--- private_dot_config/fish/conf.d/paths.fish
+++ private_dot_config/fish/conf.d/paths.fish
@@ -1,5 +1,5 @@
-set --universal fish_user_paths $fish_user_paths $HOME/.cargo/bin
-set --universal fish_user_paths $fish_user_paths $HOME/.bin
+set fish_user_paths $fish_user_paths $HOME/.cargo/bin
+set fish_user_paths $fish_user_paths $HOME/.bin
 
 
 set -a GOPATH $HOME/.go
