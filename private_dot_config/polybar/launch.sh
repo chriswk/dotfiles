8836b01b6483aad2cd793d5ee79f1d6d822a77f4
Christopher Kolstad <git@chriswk.no>
2021-01-10 20:49:32 +0100

    Update private_dot_config/polybar/launch.sh

--- private_dot_config/polybar/launch.sh
+++ private_dot_config/polybar/launch.sh
@@ -1,20 +1,9 @@
 #!/usr/bin/env sh
 
-## Add this to your wm startup file.
-
 # Terminate already running bar instances
 killall -q polybar
 
-## Wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-## Launch
-
-MONITOR=eDP1 polybar -c ~/.config/polybar/config center & 
-MONITOR=eDP1 polybar -c ~/.config/polybar/config datee & 
-MONITOR=eDP1 polybar -c ~/.config/polybar/config quicks &
-sleep 3
-MONITOR=eDP1 polybar -c ~/.config/polybar/config masked &
-MONITOR=eDP1 polybar -c ~/.config/polybar/config launcher & 
-
-
+exec polybar --reload main
