831c8df05312c5bf6d3effd83db5079611e67037
Christopher Kolstad <git@chriswk.no>
2022-04-05 08:36:15 +0200

    move to vimplug

--- run_once_12_install_vim_plug.sh
+++ run_once_12_install_vim_plug.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-if [[ -z "~/.vim/autoload/plug.vim" ]]; then
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
 
